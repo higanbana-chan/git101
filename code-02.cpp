b5568ed252a3a62d09fce4ce751af8660c2140f2
Pipatpol Wijitchaynon <pipatpol.w@ku.th>
2024-01-22 14:01:51 +0700

    Update Hello Git in C and Hello Git in C++

---
+++ code-02.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // cout ( ซี - เอ้าท์ ) = console output
+                            // endl = endline = '\n'
+    cout << "Hello Gitt" << endl; 
+}

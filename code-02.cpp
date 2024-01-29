95358991361e1ad1cf84c16a5fd063e4ccdd182d
higanbana-chan <1689richy@gmail.com>
2024-01-29 14:32:27 +0700

    update codde-02.cpp on branch feature2

--- code-02.cpp
+++ code-02.cpp
@@ -4,5 +4,5 @@ using namespace std;
 int main(){
     // cout ( ซี - เอ้าท์ ) = console output
                             // endl = endline = '\n'
-    cout << "Hello Gitt" << endl; 
+    cout << "Hello C++" << endl; 
 }
